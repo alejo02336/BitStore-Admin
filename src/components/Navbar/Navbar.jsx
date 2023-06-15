b0932d33e9d18ad95b1d3f1d0edf094185cc1ec9
damddev <alejandrom02336@gmail.com>
2023-06-14 23:27:27 -0500

    Responsive and more pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import "./navbar.css";
 
 export default function Navbar() {
+  const toggleSidebar = () => {
+    let sidebar = document.getElementById("sidebar");
+
+    sidebar.classList.add("sidebar_responsive");
+  };
   return (
     <nav className="navbar">
-      <div className="nav_icon" onclick="toggleSidebar()">
+      <div className="nav_icon" onClick={toggleSidebar}>
         <i className="fa fa-bars" aria-hidden="true"></i>
       </div>
       <div className="navbar__left">
@@ -16,7 +21,7 @@ export default function Navbar() {
       </div>
       <div className="navbar__right">
         <a href="#">
-          <img width="30px" src="../../assets/user.jpg" alt="" />
+          <img width="30px" src="assets/user.jpg" alt="" />
         </a>
       </div>
     </nav>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import "./sidebar.css";
 
 export default function Sidebar() {
+  const closeSidebar = () => {
+    let sidebar = document.getElementById("sidebar");
+
+    sidebar.classList.remove("sidebar_responsive");
+  };
+
   return (
     <div id="sidebar">
       <div className="sidebar__title">
         <div className="sidebar__img">
           <img src="assets/logo.png" alt="logo" />
-          <h1>Codersbite</h1>
+          <h1>BITSTORE</h1>
         </div>
         <i
-          onclick="closeSidebar()"
+          onClick={closeSidebar}
           className="fa fa-times"
           id="sidebarIcon"
           aria-hidden="true"
@@ -20,25 +27,25 @@ export default function Sidebar() {
       <div className="sidebar__menu">
         <div className="sidebar__link active_menu_link">
           <i className="fa fa-home"></i>
-          <a href="#">Dashboard</a>
+          <a href="#">Principal</a>
         </div>
-        <h2>MNG</h2>
+        <h2>Modulo de gestión</h2>
         <div className="sidebar__link">
           <i className="fa fa-user-secret" aria-hidden="true"></i>
-          <a href="#">Admin Management</a>
+          <a href="#">Productos</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-building-o"></i>
-          <a href="#">Company Management</a>
+          <a href="#">Carritos</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-wrench"></i>
-          <a href="#">Employee Management</a>
+          <a href="#">Ingresos</a>
         </div>
 
         <div className="sidebar__logout">
           <i className="fa fa-power-off"></i>
-          <a href="#">Log out</a>
+          <a href="#">Cerrar sesión</a>
         </div>
       </div>
     </div>
